6dadd8e7d3056eac0629e3885108b89d35beacac
Rodrigo Klein <rodrigo.klein@t2software.com.br>
2022-04-03 17:35:51 -0300

    Maven build package

---
+++ src/main/java/br/com/t2software/tt/sample/SncmSample.java
@@ -0,0 +1,13 @@
+package br.com.t2software.tt.sample;
+
+public class SncmSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello SNCM SDK Samples");
+
+	}
+
+}
+
